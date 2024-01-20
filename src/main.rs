a2fe5cd11a10b767b3d1fa1f457318cd634570ca
Cyanoptilas <februarys22@gmail.com>
2024-01-21 00:00:20 +0900

    ADD 乱数取得処理追加

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    let mut guess = String::new();
+    println!("Guess the number!");
 
-    println!("Guess the number!"); // 数を当ててごらん
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("Please input your guess."); // ほら、予想を入力してね
+    println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
+
+    println!("Please input your guess.");
 
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read line"); // 行の読み込みに失敗しました
+        .expect("Failed to read line");
 
-    println!("You guessed: {}", guess); // 次のように予想しました: {}
+    println!("You guessed: {}", guess);
 }
